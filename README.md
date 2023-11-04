b8b4b006f3be3aa7a27f710d95d80ba1f5f6ff8f
Mohanpreet <149873990+MohanpreetSN@users.noreply.github.com>
2023-11-04 14:44:37 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple Animated Login/SignUp page concept I saw on FlutterWay on Youtube
---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color login_bg = Color(0xFF9842e4);
+const Color signup_bg = Color(0xFF19c6e6);
+
+const double defpaultPadding = 16.0;
+const Duration defaultDuration = Duration(milliseconds: 300);
